c79777a6fc6d38e113546fb5f462d4bc2f3e6f43
Rinat Ahtyamov <arm.path.py@gmail.com>
2025-12-11 19:35:47 +0300

    Create model: Characteristic, CharacteristicProduct

--- app/product/models.py
+++ app/product/models.py
@@ -1,5 +1,8 @@
+from typing import Literal
+
 from slugify import slugify
 from sqlalchemy import String, event, ForeignKey, Numeric, Integer
+from sqlalchemy.dialects.postgresql import ENUM
 from sqlalchemy.orm import Mapped, mapped_column
 
 from app.settings.database import Base
@@ -19,6 +22,21 @@ class Product(Base):
     price: Mapped[float] = mapped_column(Numeric(10, 2), default=0.0)
     discount: Mapped[int] = mapped_column(Integer(), default=0)
     description: Mapped[str | None] = mapped_column(String(255), nullable=True)
+
+
+class Characteristic(Base):
+    title: Mapped[str] = mapped_column(String(255), nullable=False)
+    category_id: Mapped[int] = mapped_column(ForeignKey('category.id', ondelete='CASCADE'), nullable=False)
+    unit: Mapped[str | None] = mapped_column(String(255), nullable=True, default=None)
+    type: Mapped[Literal['integer', 'float', 'string', 'boolean']] = mapped_column(
+        ENUM('integer', 'float', 'string', 'boolean', name='enum_type_characteristic'),
+    )
+
+
+class CharacteristicProduct(Base):
+    characteristic_id: Mapped[Characteristic] = mapped_column(ForeignKey('characteristic.id', ondelete='CASCADE'))
+    product_id: Mapped[Product] = mapped_column(ForeignKey('product.id', ondelete='CASCADE'))
+    value: Mapped[str | None] = mapped_column(String(255), nullable=True)
 
 
 @event.listens_for(Category, 'before_insert')
