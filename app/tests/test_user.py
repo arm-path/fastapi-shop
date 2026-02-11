2c49f8ad8be73767f7899db7099966a930c106ab
Rinat Ahtyamov <arm.path.py@gmail.com>
2026-02-11 21:03:34 +0300

    Tests: Triggers

--- app/tests/test_user.py
+++ app/tests/test_user.py
@@ -3,7 +3,9 @@ from sqlalchemy import text
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.settings.database import test_async_session
-from app.settings.triggers import update_supplies_product_fn, update_supplies_draft_fn, update_order_is_active_fn
+from app.settings.triggers import update_supplies_product_fn, update_supplies_draft_fn, update_order_is_active_fn, \
+    supplies_product_insert_tg, supplies_product_update_tg, supplies_product_delete_tg, update_supplies_draft_tg, \
+    update_order_is_active_tg
 from app.tests import Base
 
 
@@ -20,6 +22,12 @@ async def registration_triggers(session: AsyncSession):
     await session.execute(next(update_supplies_product_fn.to_sql_statement_create_or_replace()))
     await session.execute(next(update_supplies_draft_fn.to_sql_statement_create_or_replace()))
     await session.execute(next(update_order_is_active_fn.to_sql_statement_create_or_replace()))
+
+    await session.execute(supplies_product_insert_tg.to_sql_statement_create())
+    await session.execute(supplies_product_update_tg.to_sql_statement_create())
+    await session.execute(supplies_product_delete_tg.to_sql_statement_create())
+    await session.execute(update_supplies_draft_tg.to_sql_statement_create())
+    await session.execute(update_order_is_active_tg.to_sql_statement_create())
 
 
 @pytest.mark.asyncio
