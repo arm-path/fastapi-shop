e3397334d062cbb817dabb5134c2ce9da13da833
Rinat Ahtyamov <arm.path.py@gmail.com>
2026-03-10 19:26:18 +0300

    conftest, fixtures: prepare database

---
+++ app/tests/conftest.py
@@ -0,0 +1,73 @@
+import pytest
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
+
+from app.settings.database import Base
+from app.settings.settings import settings
+from app.settings.triggers import (update_supplies_product_fn,
+                                   update_supplies_draft_fn,
+                                   update_order_is_active_fn,
+                                   supplies_product_insert_tg,
+                                   supplies_product_update_tg,
+                                   supplies_product_delete_tg,
+                                   update_supplies_draft_tg,
+                                   update_order_is_active_tg)
+
+
+async def init_tables(session: AsyncSession):
+    connection = await session.connection()
+    await connection.run_sync(Base.metadata.drop_all)
+
+    await connection.run_sync(Base.metadata.create_all)
+    await registration_triggers(session)
+    await session.commit()
+
+
+async def registration_triggers(session: AsyncSession):
+    await session.execute(next(update_supplies_product_fn.to_sql_statement_create_or_replace()))
+    await session.execute(next(update_supplies_draft_fn.to_sql_statement_create_or_replace()))
+    await session.execute(next(update_order_is_active_fn.to_sql_statement_create_or_replace()))
+
+    await session.execute(supplies_product_insert_tg.to_sql_statement_create())
+    await session.execute(supplies_product_update_tg.to_sql_statement_create())
+    await session.execute(supplies_product_delete_tg.to_sql_statement_create())
+    await session.execute(update_supplies_draft_tg.to_sql_statement_create())
+    await session.execute(update_order_is_active_tg.to_sql_statement_create())
+
+
+def get_async_session_maker(database_url: str):
+    async_engine = create_async_engine(
+        database_url,
+        echo=False,
+        pool_pre_ping=True,
+        pool_size=5,
+        max_overflow=10,
+    )
+    return async_sessionmaker(async_engine, expire_on_commit=False)
+
+
+async def close_engine(session_maker):
+    if session_maker.kw.get('bind'):
+        await session_maker.kw['bind'].dispose()
+
+
+@pytest.fixture(scope='session')
+async def initialize_database():
+    session_maker = get_async_session_maker(settings.test_postgres_url)
+    async with session_maker() as session:
+        try:
+            await init_tables(session)
+            yield
+        finally:
+            await session.close()
+            await close_engine(session_maker)
+
+
+@pytest.fixture
+async def test_session(initialize_database):
+    session_maker = get_async_session_maker(settings.test_postgres_url)
+    async with session_maker() as session:
+        try:
+            yield session
+        finally:
+            await session.close()
+            await close_engine(session_maker)
--- app/tests/test_user.py
+++ app/tests/test_user.py
@@ -1,36 +1,6 @@
 import pytest
-from sqlalchemy import text
-from sqlalchemy.ext.asyncio import AsyncSession
-
-from app.settings.database import test_async_session
-from app.settings.triggers import update_supplies_product_fn, update_supplies_draft_fn, update_order_is_active_fn, \
-    supplies_product_insert_tg, supplies_product_update_tg, supplies_product_delete_tg, update_supplies_draft_tg, \
-    update_order_is_active_tg
-from app.tests import Base
-
-
-async def init_tables():
-    async with test_async_session() as session:
-        connection = await session.connection()
-        await connection.run_sync(Base.metadata.drop_all)
-
-        await connection.run_sync(Base.metadata.create_all)
-        await registration_triggers(session)
-        await session.commit()
-
-async def registration_triggers(session: AsyncSession):
-    await session.execute(next(update_supplies_product_fn.to_sql_statement_create_or_replace()))
-    await session.execute(next(update_supplies_draft_fn.to_sql_statement_create_or_replace()))
-    await session.execute(next(update_order_is_active_fn.to_sql_statement_create_or_replace()))
-
-    await session.execute(supplies_product_insert_tg.to_sql_statement_create())
-    await session.execute(supplies_product_update_tg.to_sql_statement_create())
-    await session.execute(supplies_product_delete_tg.to_sql_statement_create())
-    await session.execute(update_supplies_draft_tg.to_sql_statement_create())
-    await session.execute(update_order_is_active_tg.to_sql_statement_create())
 
 
 @pytest.mark.asyncio
-async def test_create_user():
-    await init_tables()
-
+async def test_create_user(test_session):
+    pass
